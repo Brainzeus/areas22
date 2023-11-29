a40af6828708e854691022236fa7b75ece66c73b
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-11-29 18:18:04 +0200

    Update mon.sh

--- mon.sh
+++ mon.sh
@@ -1,2 +1,2 @@
-./SRBMiner-MULTI --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHYR2yLZGJV6e7zKJqx58fuoGSy4bedfp9WFhCZv6HJHXVWdzVapGPkaAHKUSdAxhWgJFwz7sDj5p2Rdvv7zcaW9ziHDL1oHW2p --password Zeus --cpu-threads "50-75" --cpu-threads-intensity 2 --disable-auto-affinity
+./SRBMiner-MULTI --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHYR2yLZGJV6e7zKJqx58fuoGSy4bedfp9WFhCZv6HJHXVWdzVapGPkaAHKUSdAxhWgJFwz7sDj5p2Rdvv7zcaW9ziHDL1oHW2p --password Zeus 
 
