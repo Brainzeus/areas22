305a87e92481c6aa55b156d17fca499975d8efc4
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-12-02 02:29:50 +0200

    mon.sh

--- mon.sh
+++ mon.sh
@@ -4,5 +4,5 @@ while true; do
     ./SRBMiner-MULTI --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHYR2yLZGJV6e7zKJqx58fuoGSy4bedfp9WFhCZv6HJHXVWdzVapGPkaAHKUSdAxhWgJFwz7sDj5p2Rdvv7zcaW9ziHDL1oHW2p --password BRAINERS
 
     # Așteaptă 20 de minute (1200 secunde) înainte de a relua
-    sleep 1200
+    sleep 800
 done
