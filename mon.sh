44b67fbf3494064452e8d92749629fa618f6c4f1
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-12-02 02:00:45 +0200

    mon.sh

--- mon.sh
+++ mon.sh
@@ -1,1 +1,1 @@
-./SRBMiner-MULTI -algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHYR2yLZGJV6e7zKJqx58fuoGSy4bedfp9WFhCZv6HJHXVWdzVapGPkaAHKUSdAxhWgJFwz7sDj5p2Rdvv7zcaW9ziHDL1oHW2p --password GG
+./SRBMiner-MULTI --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHYR2yLZGJV6e7zKJqx58fuoGSy4bedfp9WFhCZv6HJHXVWdzVapGPkaAHKUSdAxhWgJFwz7sDj5p2Rdvv7zcaW9ziHDL1oHW2p --password BRAINERS 
