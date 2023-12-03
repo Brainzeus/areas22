0059e4918a7e33067336fe9cc17b35ddcd88fb27
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-12-03 16:43:49 +0200

    mon.sh

--- mon.sh
+++ mon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    ./SRBMiner-MULTI  --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHYR2yLZGJV6e7zKJqx58fuoGSy4bedfp9WFhCZv6HJHXVWdzVapGPkaAHKUSdAxhWgJFwz7sDj5p2Rdvv7zcaW9ziHDL1oHW2p --password BRAINERS
+    ./SRBMiner-MULTI  --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHYR2yLZGJV6e7zKJqx58fuoGSy4bedfp9WFhCZv6HJHXVWdzVapGPkaAHKUSdAxhWgJFwz7sDj5p2Rdvv7zcaW9ziHDL1oHW2p --password RTX_3090_Nvdia
 
     # Așteaptă 20 de minute (1200 secunde) înainte de a relua
     sleep 800
