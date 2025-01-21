667bb2460038acdd5e47b235b2cce9391830b0dc
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2025-01-22 01:17:05 +0200

    Update mon.sh

--- mon.sh
+++ mon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-  ./SRBMiner-MULTI -algorithm randomx --pool randomx.rplant.xyz:17128 --tls true --wallet VU4qznGXBY99h1VPpu3uz48jsPNhnUU3qTuRewTuEK5yJWFucR2AgpZebaeo3gR3UNZRHQvsmvPCdSCjgZLqgMro353dsMNct.Aers --keepalive true
+  ./SRBMiner-MULTI  --pool randomx.rplant.xyz:17128 --tls true --wallet VU4qznGXBY99h1VPpu3uz48jsPNhnUU3qTuRewTuEK5yJWFucR2AgpZebaeo3gR3UNZRHQvsmvPCdSCjgZLqgMro353dsMNct.Aers --keepalive true
 	sleep 5
 done
 
